befedc7a9163408f4544c09f092d7c213c8c33a2
Mayra Alejandra Sierra Torres <123905946+MayraSierraAT@users.noreply.github.com>
2023-11-28 05:05:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from fastapi.responses import HTMLResponse
 import pandas as pd
 import scipy as sp
 from sklearn.metrics.pairwise import cosine_similarity
-from typing import List
 
 #instanciar la aplicación
 
@@ -137,7 +136,7 @@ async def UsersRecommend( year : int ):
 
 #Cuarta función
 @app.get("/usersworstdeveloper/{year}", name = "USERSWORSTDEVELOPER")
-async def UsersWorstDeveloper(year: int, funcion4: List[pd.DataFrame]):
+async def UsersWorstDeveloper(year: int):
     # Verificamos si el año está dentro del rango esperado
     rango_aceptado = range(2010, 2018)
     if year not in rango_aceptado:
